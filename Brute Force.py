acaf60964085577909bf23adb4ab58d1ac4cfbf9
zhaniya <zhaniyakazbekova@gmail.com>
2025-11-12 10:04:06 +0500

    all

--- Brute Force.py
+++ Brute Force.py
@@ -8,16 +8,24 @@ points = {
 I = list(points.keys())
 J = list(points.keys())
 p = 2
-def dist(a, b):
+def dist(a,b):
     ax, ay = points[a]; bx, by = points[b]
-    return math.hypot(ax - bx, ay - by)
+    return math.hypot(ax-bx, ay-by)
 d = { (i,j): dist(i,j) for i in I for j in J }
 best_R = float('inf')
 best_S = None
+best_assign = None
 for S in itertools.combinations(J, p):
-    worst = max(min(d[(i,j)] for j in S) for i in I)
+    assign = {}
+    worst = 0.0
+    for i in I:
+        nearest = min((d[(i,j)], j) for j in S)
+        assign[i] = nearest[1]
+        worst = max(worst, nearest[0])
     if worst < best_R:
         best_R = worst
         best_S = S
+        best_assign = assign
 print("Best sites:", best_S)
 print("Worst distance R:", round(best_R, 3))
+print("Assignments:", best_assign)
--- PuLP.py
+++ PuLP.py
@@ -1,18 +1,15 @@
-import pulp as pl, math
-
-coords = {"A":(0,0),"B":(0,4),"C":(3,0),"D":(6,4)}
-I, J = coords.keys(), coords.keys()
+import pulp as pl
+I = ["A","B","C","D"]
+J = ["A","B","C","D"]
 p = 2
-
+coords = {"A":(0,0),"B":(0,4),"C":(3,0),"D":(6,4)}
+import math
 d = {(i,j): math.hypot(coords[i][0]-coords[j][0], coords[i][1]-coords[j][1]) for i in I for j in J}
-
 m = pl.LpProblem("p_center", pl.LpMinimize)
-x = pl.LpVariable.dicts("x", (I,J), 0, 1, pl.LpBinary)
-y = pl.LpVariable.dicts("y", J, 0, 1, pl.LpBinary)
-R = pl.LpVariable("R", lowBound=0)
-
+x = pl.LpVariable.dicts("x", (I,J), lowBound=0, upBound=1, cat="Binary")
+y = pl.LpVariable.dicts("y", J, lowBound=0, upBound=1, cat="Binary")
+R = pl.LpVariable("R", lowBound=0, cat="Continuous")
 m += R
-
 for i in I:
     m += pl.lpSum(x[i][j] for j in J) == 1
 for i in I:
@@ -20,9 +17,11 @@ for i in I:
         m += x[i][j] <= y[j]
 m += pl.lpSum(y[j] for j in J) == p
 for i in I:
-    m += pl.lpSum(d[(i,j)]*x[i][j] for j in J) <= R
-
+    m += pl.lpSum(d[(i,j)] * x[i][j] for j in J) <= R
 m.solve(pl.PULP_CBC_CMD(msg=False))
-sol = [j for j in J if pl.value(y[j])>0.5]
-print("Оптимальные станции:", sol)
-print("Макс. расстояние R =", round(pl.value(R),3))
+sol_y = [j for j in J if pl.value(y[j]) > 0.5]
+R_val = pl.value(R)
+assign = {i: max(J, key=lambda j: pl.value(x[i][j])) for i in I} 
+print("MILP sites:", sol_y)
+print("R:", round(R_val, 3))
+print("Assignments:", assign)
